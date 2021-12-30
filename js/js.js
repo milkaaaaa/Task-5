37a7b53ff5c72e6b3c476174640075a443dea732
milkaaaaa <95637433+milkaaaaa@users.noreply.github.com>
2021-12-30 13:34:01 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
-
+send.addEventListener("click",calc);
+function calc(){
+    
+    let val_tovaroob = tovaroob.value;
+    let val_nadbavka = nadbavka.value;
+    let val_sebestoimost = sebestoimost.value;
+    let val_result;
+    
+    val_result = val_tovaroob*(val_nadbavka/100)-val_sebestoimost;
+    
+    result.innerHTML = val_result;
+}
